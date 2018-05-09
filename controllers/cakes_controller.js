69123d4e575c5e3707c030bec2e7530ab23313b8
Liana Mancini <liana.mancini@gmail.com>
2018-05-09 17:00:46 -0400

    add controllers, views, server

---
+++ controllers/cakes_controller.js
@@ -0,0 +1,35 @@
+var express = require("express");
+var router = express.Router();
+
+var cake = require("../models/cake.js");
+
+router.get("/", (req, res) => {
+    cake.selectAll( data => {
+        var cakeObj = { cakes: data };
+
+        res.render("index", cakeObj);
+    })
+});
+
+router.post("/api/cakes", (req, res) => {
+    cake.insertOne(["cake_name", "eaten"], [req.body.cake_name, req.body.eaten], result => {
+        console.log(result);
+        res.json(result);
+    });
+});
+
+router.put("/api/cats/:id", (req, res) => {
+    var condition = "id = " + req.params.id;
+
+    cake.updateOne({
+        eaten: req.body.eaten
+    }, condition, result => {
+        if (result.changedRows === 0){
+            return res.status(404).end();
+        } else {
+            res.status(200).end();
+        }
+    })
+})
+
+module.exports = router;
