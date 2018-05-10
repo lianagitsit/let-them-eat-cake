5a9250ea868cf38e72d8b5bd99490c7e5807ef96
Liana Mancini <liana.mancini@gmail.com>
2018-05-09 23:03:36 -0400

    wire up JawsDB connection:

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 require("dotenv").config();
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: process.env.DB_PASS,
-  database: "cake_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL){
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    port: 3306,
+    host: "localhost",
+    user: "root",
+    password: process.env.DB_PASS,
+    database: "cake_db"
+  });
+}
 
 connection.connect(function(err) {
   if (err) {
